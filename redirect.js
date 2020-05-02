561be220b94f34efd9e5168fd4e9f9b2dd80316a
Lars S. Peder <lars@peder.cc>
2020-05-03 00:00:40 +0200

    Only pass q parameter

--- redirect.js
+++ redirect.js
@@ -1,1 +1,3 @@
-window.location.href = `https://duckduckgo.com/${window.location.search}`;
+const params = new URLSearchParams(window.location.search);
+if (params.has("q"))
+  window.location.href = `https://duckduckgo.com/${params.get("q")}`;
